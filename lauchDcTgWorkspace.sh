4623fb3255ac1d8c0a52cfa93738b246f6570afe
ileskanRepot <ileska@tutanota.com>
2022-02-25 10:15:08 +0200

    changes

--- lauchDcTgWorkspace.sh
+++ lauchDcTgWorkspace.sh
@@ -3,5 +3,10 @@ i3-msg "workspace DcTg;append_layout ~/.config/i3/workspace_DcTg.json"
 discord & 
 telegram-desktop &
 kitty &
-brave &
-brave music.youtube.com &
+chromium &
+
+i3-msg 'gaps inner current set 20'
+i3-msg 'gaps outer current set -20'
+# i3-msg '[class=^discord$] focus;floating auto_off;border pixel 2'
+sleep 10
+i3-msg '[class=^discord$] border pixel 2'
